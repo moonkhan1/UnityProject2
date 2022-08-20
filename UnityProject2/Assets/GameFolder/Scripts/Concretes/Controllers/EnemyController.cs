ef059bd6315e353c88538a99e60d7fb1bf889e61
moonkhan1 <ayxanhesenov3105@gmail.com>
2022-08-20 16:54:20 +0400

    Enemt destroy itself in out of range

--- UnityProject2/Assets/GameFolder/Scripts/Concretes/Controllers/EnemyController.cs
+++ UnityProject2/Assets/GameFolder/Scripts/Concretes/Controllers/EnemyController.cs
@@ -6,8 +6,10 @@ using UnityProject2.Movements;
 namespace UnityProject2.Controllers{
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] float _maxLifeTime = 10f;
     [SerializeField] float _moveSpeed = 10f;
     VerticalMover _mover;
+    float _currentLifeTime = 0f;
 
     public float MoveSpeed => _moveSpeed;
 
@@ -15,8 +17,21 @@ public class EnemyController : MonoBehaviour
         _mover = new VerticalMover(this);
     }
 
+    private void Update() {
+        _currentLifeTime += Time.deltaTime;
+        if(_currentLifeTime > _maxLifeTime)
+        {
+            _currentLifeTime = 0f;
+            KillYourself();
+        }
+    }
+
     private void FixedUpdate() {
         _mover.FixedTick();
+    }
+
+    void KillYourself(){
+        Destroy(this.gameObject);
     }
 }
 }
