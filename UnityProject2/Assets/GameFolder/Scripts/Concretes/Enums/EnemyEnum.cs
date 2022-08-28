34af8ba929a7729b46740a0b3afa4d913a73f5af
moonkhan1 <ayxanhesenov3105@gmail.com>
2022-08-28 08:10:28 -0700

    Enemies added to the Enum

---
+++ UnityProject2/Assets/GameFolder/Scripts/Concretes/Enums/EnemyEnum.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityProject2.Enums{
+
+public enum EnemyEnum 
+{
+    // Dusmenlerin tiplerine gore adlandirilmasi ve isdifade edilmesi
+    Standard, //Enemy1
+    Height, //Enemy2....
+    Width,
+    HeightWidth 
+
+}
+}
--- UnityProject2/Assets/GameFolder/Scripts/Concretes/Managers/EnemyManager.cs
+++ UnityProject2/Assets/GameFolder/Scripts/Concretes/Managers/EnemyManager.cs
@@ -1,15 +1,20 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityProject2.Controllers;
 using UnityProject2.Abstracts.Utilities;
+using UnityProject2.Enums;
 
 namespace UnityProject2.Managers{
 public class EnemyManager : SingletonMonoBehaviorObject<EnemyManager>
 {
     // Birden cox Enemy obyektini elave eded bilmek ucun bu deyiseni List edirik
+    
     [SerializeField] EnemyController[] _enemyPrefebs;
-    Queue<EnemyController> _enemies = new Queue<EnemyController>(); // Enemy Pool
+
+    Dictionary<EnemyEnum, Queue<EnemyController>> _enemies = new Dictionary<EnemyEnum,Queue<EnemyController>>(); // Enemy Pool
+
 
     private void Awake() {
         SingletonThisObject(this);
@@ -20,13 +25,20 @@ public class EnemyManager : SingletonMonoBehaviorObject<EnemyManager>
 
     private void InitializePool()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < _enemyPrefebs.Length; i++)
         {
-            EnemyController newEnemy = Instantiate(_enemyPrefebs[Random.Range(0,_enemyPrefebs.Length)]);
-            newEnemy.gameObject.SetActive(false);
-            newEnemy.transform.parent = this.transform;
-            _enemies.Enqueue(newEnemy); // Enemy Poola elave edirik
+            Queue<EnemyController> enemyController = new Queue<EnemyController>();
+            for (int j = 0; j < 10; j++)
+            {
+                EnemyController newEnemy = Instantiate(_enemyPrefebs[i]);
+                newEnemy.gameObject.SetActive(false);
+                newEnemy.transform.parent = this.transform;
+                _enemies.Enqueue(newEnemy); // Enemy Poola elave edirik
+                _enemies.Enqueue(newEnemy);
+            }    
+            _enemies.Add((EnemyEnum)i,enemyController);
         }
+        
     }
     public void SetPool(EnemyController enemyController)
     {
